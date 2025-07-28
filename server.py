4067291f396a03ff5f323f55e0103dff81921f17
herman-gg <160828235+herman-gg@users.noreply.github.com>
2025-07-28 09:19:44 +0000

    feat: Add ip and hostname to the template and status;

--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 from flask import Flask, jsonify, render_template
 from datetime import datetime
 import os
+import socket
 
 app = Flask(__name__)
 
-@app.route('/status', methods=['GET'])
-def status():
+def get_status_data():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
     status_data = {
         'timestamp': datetime.utcnow().isoformat() + 'Z',
-        'pod': os.getenv('POD_NAME', default='None')
+        'pod': os.getenv('POD_NAME', default='None'),
+        'ip': f'{hostname} : {ip}',
     }
-    return jsonify(status_data)
+    return status_data
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify(get_status_data())
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    data = get_status_data()
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', default=8080))
